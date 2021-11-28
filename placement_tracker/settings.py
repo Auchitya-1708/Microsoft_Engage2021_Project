aaa8bdf41929180df535711d3f2df1ba1b45f80f
Auchitya-1708 <95166669+Auchitya-1708@users.noreply.github.com>
2021-11-28 17:51:45 +0530

    Update settings.py

--- placement_tracker/settings.py
+++ placement_tracker/settings.py
@@ -21,11 +21,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+2+fesq$99ktugzqb1+pc%7md7(a70uf6t!d#5=l=m&5skxej'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
